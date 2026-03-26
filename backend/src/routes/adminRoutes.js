1f5d8c3537182724e9dab1e6033c92fd53b19729
TGD82 <goplanidhir@outlook.com>
2026-03-27 03:51:48 +0530

    Added role based middleware

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -12,22 +12,23 @@ import {
 } from "../controllers/adminController.js";
 
 import authMiddleware  from "../middleware/authMiddleware.js";
+import roleMiddleware from "../middleware/roleMiddleware.js";
 
-router.put("/change-password", authMiddleware, changePassword);
+router.put("/change-password", authMiddleware, roleMiddleware("Admin"), changePassword);
 
-router.put("/update-fare", authMiddleware, updateFare);
+router.put("/update-fare", authMiddleware, roleMiddleware("Admin"), updateFare);
 
-router.get("/get-fare", authMiddleware, getFare);
+router.get("/get-fare", authMiddleware, roleMiddleware("Admin"), getFare);
 
-router.post("/create-admin", authMiddleware, createAdmin);
+router.post("/create-admin", authMiddleware, roleMiddleware("Admin"), createAdmin);
 
-router.get("/get-users", authMiddleware, getUsers);
+router.get("/get-users", authMiddleware,roleMiddleware("Admin"), getUsers);
 
-router.get("/get-admins", authMiddleware, getAdmins);
+router.get("/get-admins", authMiddleware,roleMiddleware("Admin"), getAdmins);
 
-router.get("/user/:id/journeys", authMiddleware, getUserJourneys);
+router.get("/user/:id/journeys", authMiddleware,roleMiddleware("Admin"), getUserJourneys);
 
-router.get("/dashboard-stats", authMiddleware, getDashboardStats);
+router.get("/dashboard-stats", authMiddleware,roleMiddleware("Admin"), getDashboardStats);
 
 
 export default router;
