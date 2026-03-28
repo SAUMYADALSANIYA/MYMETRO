442ecdc09bd28b20f753e44b25314a5e39d7a86e
HaneeJoshi <haneejoshi16@gmail.com>
2026-03-28 16:45:36 +0530

    ui

--- backend/src/routes/ticketRouts.js
+++ backend/src/routes/ticketRouts.js
@@ -4,16 +4,32 @@ import Ticket from "../models/ticket.js";
 
 const router = express.Router();
 
-// GET USER TICKETS
 router.get("/history", authMiddleware, async (req, res) => {
   try {
     const tickets = await Ticket.find({ boughtBy: req.user.id })
       .sort({ createdAt: -1 });
 
-    res.json({ tickets });
+    const updatedTickets = tickets.map((t) => {
+      const createdTime = new Date(t.createdAt).getTime();
+      const now = Date.now();
+
+      const hoursPassed = (now - createdTime) / (1000 * 60 * 60);
+
+      let computedStatus = t.status;
+
+      if (t.status === "ACTIVE" && hoursPassed > 12) {
+        computedStatus = "INACTIVE";
+      }
+
+      return {
+        ...t._doc,
+        computedStatus
+      };
+    });
+
+    res.json({ tickets: updatedTickets });
   } catch (err) {
     res.status(500).json({ message: "Failed to fetch history" });
   }
 });
-
 export default router;
